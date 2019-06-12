7ac702c9929459a71e9f6ce156fe3df4e84fede9
mynameisjenn <jenniferlynnhatton@gmail.com>
2019-06-11 20:01:20 -0500

    updated router.js

--- src/router.js
+++ src/router.js
@@ -1,24 +1,24 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import Signup from './views/Signup.vue';
+import Login from './views/Login.vue';
+import Logout from './views/Logout.vue';
+import Events from './views/Events.vue';
+import Band from './views/Band.vue';
 
-
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+    { path: '/', name: 'home', component: Home},
+    { path: '/events', name: 'events', component: Events},
+    { path: '/band', name: 'band', component: Band},
+
+    { path: '/signup', name: 'signup', component: Signup},
+    { path: '/login', name: 'login', component: Login},
+    { path: '/logout', name: 'logout', component: Logout}
+    
+
   ]
-})
+});
